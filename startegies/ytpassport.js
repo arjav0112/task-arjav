2f8989a84869321ce0039f4ac8618ad203289f3e
Arjav Jain <jarjav2005@gmail.com>
2024-09-16 20:14:26 +0530

    callback link changed

--- startegies/ytpassport.js
+++ startegies/ytpassport.js
@@ -3,38 +3,6 @@ const { Strategy: googleStrategy } = require("passport-google-oauth20")
 const User = require('../models/user.js')
 const { Strategy: GitHubStrategy } = require("passport-github2")
 
-passport.use(new GitHubStrategy({
-    clientID: process.env.CLIENT_ID_GITHUB,
-    clientSecret: process.env.CLIENT_SECRET_GITHUB,
-    callbackURL: "http://localhost:3001/auth/github",
-    scope: ['user:email']
-},async (accessToken,refreshToken,profile,done)=>{
-    try {
-            
-        let user = await User.findOne({ email: profile.emails[0].value });
-
-        if (user) {   
-            let id = user["_id"]
-            user = await User.findByIdAndUpdate(id,{name:profile.username,TokenGit:accessToken},{new:true}); 
-            return done(null, user);
-        }
-        
-        user = new User({
-            name: profile.username,
-            email: profile.emails[0].value,
-           
-            TokenGit: accessToken,
-        });
-
-        await user.save();
-
-        done(null,user);
-    } catch (error) {
-        console.error(error);
-        done(error, null);
-    }
-}))
-
 passport.use(new googleStrategy(
     {
         clientID: process.env.CLIENT_ID,
